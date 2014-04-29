656cce83179f8d64f32db2b55ae6a5a6714763ec
Christian Schirin <Christian.Schirin@haw-hamburg.de>
2014-04-29 11:28:49 +0200

    Commit nach ITS-Praktikum 2, TripleDES: die implementierung des betriebsmodus CFB ist falschrum. Statt vom ende der datei zu lesen einfach die chiffretextblöcke hintereinander zu klartextblöcken entschlüsseln!!! -Listen rausschmeißen -Vielleicht objektorientiert machen? (Huebner bemängelt fehlende kapselung)
    -Eventuell bei HC1 while-schleife einbauen

--- src/LCG.java
+++ src/LCG.java
@@ -1,57 +1,27 @@
-import java.util.Random;
 
-/**
- * Created by Christian on 26.04.2014.
- *
- * IT-Sicherheit Praktikum 2 Aufgabe 1
- * Implementierung eines Linearen Kongruenzgenerators
- *
- * Die parameter der fortschaltfunktion sind dem
- * computer-algebra-system DERIVE nachempfunden.
- *
- */
+
+
+//Pseudozufallszahlgenerator
 public class LCG {
-    //Konstanten
 
-    //für die Lineare kongruenzmethode
-    private static final long A = 3141592653L;
-    private static final long B = 1L;
+	
+	private long x;
 
-    private static final long M = 4294967296L; // Soll 2 ** 32 sein
+	public LCG(long seed) {
+		this.x = seed;
+	}
 
-    //sonstige
-    //ein möglicher startwert
-    public static final long DEFAULT_SEED = 42 + 23;
+	public long nextValue() {
+		//Variablen a,b,m sind feste Werte aus PDF!
+		long a = 2147001325;
+		long b = 715136305;
+		long m = (long) Math.pow(2,32);
+		
+		//Berechnet Zufallszahl und benutzt diese um neue zu berechnen.
+		x = (a * x + b) % m;
 
-    //Der seed bzw später der wert für die fortschaltfunktion aus der fortschaltfunktion
-    private long x;
+		return x;
 
-    public LCG(long startValue) {
-        this.x = startValue;
-    }
-
-
-    /** gibt eine zufällige 64-bit zahl zurück
-     *
-     * @return a random long integer
-     */
-    public long nextValue() {
-        //x(k+1) berechnen
-        long nextX = (A * x + B) % M;
-
-        //x ersetzen
-        this.x = nextX;
-
-        //x zurückgeben;
-        return this.x;
-    }
-
-    public static void main(String[] args) {
-        LCG mine = new LCG(DEFAULT_SEED);
-        Random java = new Random(DEFAULT_SEED);
-        for(int i = 0; i < 10; i++) {
-            System.out.println("Erzeugte Zufallszahl von LCG: " + mine.nextValue() + " von java.util.Random: " + java.nextInt());
-        }
-    }
+	}
 
 }
